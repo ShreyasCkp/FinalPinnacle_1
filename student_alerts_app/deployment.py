25720c114afbbfc6ee95b7495ab8f2337bf29e9b
ShreyasCkp <shreyas@ckpsoftware.com>
2025-10-23 23:16:09 +0530

    .

--- student_alerts_app/deployment.py
+++ student_alerts_app/deployment.py
@@ -1,10 +1,11 @@
 import os
 from decouple import config
+from .settings import *
 
 
 DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='college-portal-efdhgwg8eqg0ejaj.centralindia-01.azurewebsites.net').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='pinnacle-college-final-gwdbf8dvfcetgmef.centralindia-01.azurewebsites.net').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
     f"https://{host}" for host in ALLOWED_HOSTS if host not in ['localhost', '127.0.0.1']
