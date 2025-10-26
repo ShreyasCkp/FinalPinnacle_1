e1ca39b3bf7828861b2753f49ede00beed5cee5c
ShreyasCkp <shreyas@ckpsoftware.com>
2025-10-26 21:37:07 +0530

    .

---
+++ master/migrations/0002_excelupload_delete_studentupload.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('master', '0001_initial'),
+    ]
+
+    operations = [
+        # No-op, because the actual model/table already exists or was deleted manually
+    ]
